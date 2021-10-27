d72ae93c5f1979cde1e75d9dd8725368334b84c0
Sharipkhan Almas <alma.iceee@gmail.com>
2021-10-27 09:34:39 +0600

    First Commit

---
+++ IceCreamShop/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamShop.Models
+{
+    public class Company : BaseEntity
+    {
+        public ICollection<IceScream> IceScreams { get; set; }
+    }
+}
---
+++ IceCreamShop/Models/IceScream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamShop.Models
+{
+    public class IceScream : BaseEntity
+    {
+        public int Price { get; set; }
+        public Company Company { get; set; }
+    }
+}
